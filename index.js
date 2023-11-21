c767717ac301e38439a63085cf480d03f8051feb
Rishikeshari7 <rishikeshari7@gmail.com>
2023-11-21 15:43:29 +0530

    added js code to render data

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
 const userImg=document.querySelector("[data-userImg]");
-const Name=document.querySelector("[data-name]");
-const userName=document.querySelector("[data-userName]");
+const userKaName=document.querySelector("[data-name]");
+const gitUserName=document.querySelector("[data-userName]");
 const joinedDate = document.querySelector("[data-Date]");
 const bio=document.querySelector("[data-bio]");
 const repos=document.querySelector("[data-repos]")
 const followers=document.querySelector("[data-followers]");
 const following=document.querySelector("[data-following]");
-const location=document.querySelector("[data-location]");
+const userLocation=document.querySelector("[data-location]");
 const twitter=document.querySelector("[data-twitter]");
 const link =document.querySelector("[data-link]");
 const business=document.querySelector("[data-business]");
+const searchForm=document.querySelector(".form-container");
 
+
+const searchInput=document.querySelector("[data-searchInput]");
+searchForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let userName=searchInput.value;
+    fetchUserData(userName);
+   
+});
+
+async function fetchUserData(userName){
     
+    try{
+        console.log(userName);
+        const response = await fetch (`https://api.github.com/users/${userName}`);
+        const data=await response.json();
+        console.log(data.id);
+        if(!response.ok){
+            console.log("User Doesnt't exist ");
+        }
+        else{
+            renderUserData(data);
+        } 
+    }
 
-const api_URL=`https://api.github.com/user/${userName}`;
+    catch(err){
+        console.log("error in function fetchUserData",err);
+    }
+}
+
+async function renderUserData(data){
+    userImg.src=data.avatar_url;
+    userKaName.innerHTML=data.name;
+    gitUserName.innerHTML=`@${data.login}`;
+    joinedDate.innerHTML=`Joined On: ${new Date().toDateString(data.created_at)}`;
+    bio.innerHTML=data.bio||"This Profile has no Bio";
+    repos.innerHTML=data.public_repos||'0';
+    followers.innerHTML=data.followers||'0';
+    following.innerHTML=data.following||'0';
+    userLocation.innerHTML=data.location||'Not Available';
+    twitter.innerHTML=data.twitter_username||'Not Available';
+    link.innerHTML=data.email||'Not Available';
+    business.innerHTML=data.company||'Not Available';
+}
