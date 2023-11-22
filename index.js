f0b11de565e97d16c2820a4300b1db2a90427e8d
Rishikeshari7 <rishikeshari7@gmail.com>
2023-11-22 14:20:28 +0530

    added style

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function renderUserData(data){
     userImg.src=data.avatar_url;
     userKaName.innerHTML=data.name;
     gitUserName.innerHTML=`@${data.login}`;
-    joinedDate.innerHTML=`Joined On: ${new Date().toDateString(data.created_at)}`;
+    joinedDate.innerHTML=` ${new Date().toDateString(data.created_at)}`;
     bio.innerHTML=data.bio||"This Profile has no Bio";
     repos.innerHTML=data.public_repos||'0';
     followers.innerHTML=data.followers||'0';
